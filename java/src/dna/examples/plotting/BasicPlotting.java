1ce605ba826324e0eeb39adbdcd0e7bd6cba41b1
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2016-01-06 21:38:45 +0100

    added commentary to BasicPlotting example

--- java/src/dna/examples/plotting/BasicPlotting.java
+++ java/src/dna/examples/plotting/BasicPlotting.java
@@ -19,7 +19,29 @@ import dna.updates.generators.BatchGenerator;
 import dna.updates.generators.random.RandomBatch;
 import dna.util.Config;
 
-// TODO write
+/**
+ * 
+ * This class provides a simple demonstration of the plotting available in DNA.<br>
+ * 
+ * First the config is set to zip all generated runs. Then all necessary objects
+ * for the generation are being prepared:<br>
+ * 
+ * A GraphGenerator, which generates a random directed graph with 100 nodes and
+ * 200 edges.<br>
+ * 
+ * A BatchGenerator, which randomly adds 20 and removes 10 edges on each update.<br>
+ * 
+ * An array of metrics, which will be calculated in each batch. Namely a
+ * DegreeDistribution and a DirectedClusteringCoefficient.<br>
+ * 
+ * Then the actual generation takes place with 1 run and 10 batches. Lastly the
+ * generated series will be plotted to the plotDir. This is the most basic way
+ * to plot data.
+ * 
+ * 
+ * @author Rwilmes
+ * 
+ */
 public class BasicPlotting extends Example {
 
 	public BasicPlotting(String[] args) {
