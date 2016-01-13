dfc43e8ad961d2976a4a34603ff7a95005d507ee
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2016-01-13 19:02:47 +0100

    added LiveVis-example template

---
+++ java/src/dna/examples/vis/LiveVisualization.java
@@ -0,0 +1,15 @@
+package dna.examples.vis;
+
+// TODO
+public class LiveVisualization extends Vis {
+
+	public LiveVisualization(String[] args) {
+		super(args);
+	}
+
+	@Override
+	protected void execute() {
+		// TODO
+	}
+
+}
